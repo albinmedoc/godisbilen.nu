0aa144b1a9536b731b5c5d40e5ece2553713aec2
Albin Médoc <stamline203@gmail.com>
2020-05-23 13:05:11 +0200

    Load envirement variables

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Load env
+dotenv.config({
+    path: './config.env',
+});
 
 const app = express();
 
